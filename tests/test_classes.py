b75223f8d41cd32354cfdd93ab47810ab581f80d
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-03-19 20:21:20 +0500

    add tests for new functions

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,8 +1,6 @@
-import sys
-
 import pytest
 
-from units.classes import Category, Product
+from units.classes import Category, Product, ProductIteration
 
 
 @pytest.fixture()
@@ -20,11 +18,13 @@ def test_category_init(category_technics):
 
     assert category_technics.get_products == ['Телевизор, 123000.0 руб. Остаток: 7 шт.',
                                               'Компьютер, 999000.0 руб. Остаток: 3 шт.']
+    assert len(category_technics) == 10
+    assert str(category_technics) == 'Техника, количество продуктов: 10 шт.'
 
 
 @pytest.fixture()
 def product():
-    return Product("Компьютер", "Игровой пк без видеокарты", 48990, 3)
+    return Product("Компьютер", "Игровой пк без видеокарты", 48990.0, 3)
 
 
 def test_product_init(product):
@@ -33,17 +33,35 @@ def test_product_init(product):
     assert product.price == 48990
     assert product.quantity == 3
 
+
+def test_product_price_setter(product):
     product.price = 50000
     assert product.price == 50000
 
     product.price = -123
     assert product.price == 50000
 
-    assert product.add_product(
-        {"name": "Наушники", "description": "Норм звук", "price": 1500.0, "quantity": 2}).name == "Наушники"
-    assert product.add_product(
-        {"name": "Наушники", "description": "Норм звук", "price": 1500.0, "quantity": 3}).description == "Норм звук"
-    assert product.add_product(
-        {"name": "Наушники", "description": "Норм звук", "price": 1500.0, "quantity": 4}).price == 1500.0
-    assert product.add_product(
-        {"name": "Наушники", "description": "Норм звук", "price": 1500.0, "quantity": 2}).quantity == 11
+
+def test_product_add_product(product):
+    new_product = product.add_product({"name": "Наушники", "description": "Норм звук", "price": 1500.0, "quantity": 2})
+    product.add_product({"name": "Наушники", "description": "Норм звук", "price": 1600.0, "quantity": 4})
+    assert new_product.name == "Наушники"
+    assert new_product.description == "Норм звук"
+    assert new_product.price == 1600.0
+    assert new_product.quantity == 6
+
+
+def test_product_add(product):
+    product2 = Product("Телефон", "Работает", 15990.0, 5)
+    assert product + product2 == 226920.0
+
+
+@pytest.fixture()
+def product_iteration():
+    return ProductIteration(["Телефон", "Наушники", "Компьютер"])
+
+
+def test_iteration_init(product_iteration):
+    assert product_iteration.category == ["Телефон", "Наушники", "Компьютер"]
+    product_list = [i for i in product_iteration]
+    assert product_list == ["Телефон", "Наушники", "Компьютер"]
