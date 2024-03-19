e99e5d634449e0d3e4d15d9f07ea55c756ee6250
Emmanuil Galihanov <emikgal2507@gmail.com>
2024-03-19 20:21:00 +0500

    add functions from lesson 3

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-from units.classes import Product
+from units.classes import Product, ProductIteration
 from units.read_json import read_json
 
 products_list = read_json()
 
-
-# prod1 = Product("Советский", "Может быть работает", 1500.0, 2)
-# products_list[1].add_product(Product("Советский", "Может быть работает", 1500.0, 2))
-# products_list[1].add_product(Product("Советский", "Может быть работает",2000.0, 1))
+print(str(products_list[1]))
 
 products_list[1].add_product(Product.add_product({"name": "Советский", "description": "Может быть работает",
                                                   "price": 1500.0, "quantity": 2}))
@@ -19,18 +16,28 @@ products_list[1].add_product(Product.add_product({"name": "новый", "descrip
 for i in products_list[1].get_products:
     print(i)
 
+print(len(products_list[1]))
+
+new = products_list[1]._products[1] + products_list[1]._products[2]
+
+print(new)
+
+tvs = ProductIteration(products_list[1]._products)
+print(list(tvs))
+
+for i in tvs:
+    print(i)
+
+print(str(products_list[1]))
 
 # products_list[0].add_product(Product.add_product({}, 12, 3))
-
+#
 # print(products_list[0]._products[1].price)
 # products_list[0]._products[1].price = float(input("Введите новую цену "))
 # print(products_list[0]._products[1].price)
-
-print(products_list[1]._products[1]._price)
-
-products_list[1]._products[1].price = 4000
-
-print(products_list[1]._products[1]._price)
-
-
-
+#
+# print(products_list[1]._products[1]._price)
+#
+# products_list[1]._products[1].price = 4000
+#
+# print(products_list[1]._products[1]._price)
